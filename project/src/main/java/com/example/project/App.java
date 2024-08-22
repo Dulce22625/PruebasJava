8ab96b4865ac64197a2f698a76c7611df96d5919
Dulce18san <al222010120@gmail.com>
2024-08-22 15:45:41 -0600

    Primer commit

---
+++ project/src/main/java/com/example/project/App.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+
+public class App 
+{
+        public static void main( String[] args )
+    {
+       
+        //Interfaz1 myClasInterfaz1 = new Interfaz1() {
+            
+            
+        };
+    }
+
---
+++ project/src/main/java/com/example/project/Interfaz1.java
@@ -0,0 +1,10 @@
+package com.example.project;
+
+public interface Interfaz1 {
+
+    void metodo1();
+//metodo aqui
+//en otra clase mando a llama con implements
+//en el main mando a llamar la interfaz
+
+}
